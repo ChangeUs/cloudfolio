ef67b1f66b12520c2349af61d249a2b334a95898
HyunSeo Nam <auspice7g@gmail.com>
2017-10-18 12:51:59 +0900

    Add signup(url, view, form)

--- account/models.py
+++ account/models.py
@@ -33,46 +33,17 @@ class AccountManager(BaseUserManager):
 
 # class Account(AbstractBaseUser, TimeStampedModel):
 class Account(AbstractBaseUser):
-    username = models.CharField(max_length=30, default="", unique=True)
+    name = models.CharField(max_length=50, default="", unique=True)
     email = models.EmailField(max_length=255, default="", unique=True)
-    is_admin = models.BooleanField(default=False)
+
     is_active = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    name = models.CharField(max_length=100, default="")
 
     objects = AccountManager()
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email', 'name']
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
 
     def __str__(self):
         return "username: " + self.username
-
-    @property
-    def is_staff(self):
-        "Is the user a member of staff?"
-        # Simplest possible answer: All admins are staff
-        return self.is_admin
-
-    def email_user(self, subject, message, from_email=None):
-        """ Used for django-registration """
-        send_mail(subject, message, from_email, [self.email])
-
-    def has_module_perms(self, app_label):
-        """
-        Returns True if the user has any permissions in the given app label.
-        Uses pretty much the same logic as has_perm, above.
-        """
-        # Active superusers have all permissions.
-        if self.is_active and self.is_superuser:
-            return True
-        return auth_models._user_has_module_perms(self, app_label)
-
-    def has_perm(self, perm, obj=None):
-        if self.is_active and self.is_superuser:
-            return True
-
-        return auth_models._user_has_perm(self, perm, obj)
-
-    def get_short_name(self):
-        return self.name
--- account/views.py
+++ account/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from account.forms import UserCreationForm
 
-# Create your views here.
+# 회원가입
+def signup(request):
+
+    # 기로그인 -> redirect
+    if request.user.is_anonymous:
+        pass
+    elif request.user:
+        return redirect('/')
+
+    template = 'account/signup.html'
+    signupForm = UserCreationForm()
+    message = ""
+
+    # form 작성 후 post 액션 시
+    if request.method == "POST":
+        signupForm = UserCreationForm(request.POST, request.FILES or None)
+        if signupForm.is_valid():
+            user = signupForm.save(commit=False)
+            user.save()
+            # TODO: 이메일 인증 기능 추가 필요
+            return HttpResponse('회원가입 완료')
+        else:
+            message="패스워드 미일치"
+
+    elif request.method == "GET":
+        pass
+
+    context = {"signupForm" : signupForm, "message": message}
+    return render(request, template, context)
