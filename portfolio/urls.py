9a77956a7682b1595a732f2506ab2835b417fa77
HyunSeo Nam <auspice7g@gmail.com>
2017-10-18 20:14:09 +0900

    Add base html for frontend development(temp)

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login, logout, password_change, password_reset
-from account.views import signup
+from portfolio.views import portfolioIndexView, portfolioBaseView
 
 urlpatterns = [
-
+    # TODO: url 백단에서 수정필요.
+    # 프론트엔드를 위한 임시.
+    url(r'^index/$', portfolioIndexView, name='index'),
+    url(r'^base/$', portfolioBaseView, name='base'),
 ]
