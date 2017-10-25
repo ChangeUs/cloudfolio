61e20f15a3b310ac3ea876e41b430092c1ae446e
HyunSeo Nam <auspice7g@gmail.com>
2017-10-26 05:14:53 +0900

    Set success message when login

--- portfolium/urls.py
+++ portfolium/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^portfolios/', include('portfolio.urls', namespace='portfolios')),
     url(r'^accounts/', include('account.urls', namespace='accounts')),
-    url(r'^', signin),
+    url(r'^$', signin),
 ]
