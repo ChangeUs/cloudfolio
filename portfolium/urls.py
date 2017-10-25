28380ecca787d4bf16d2b07fe92fadda9f94c828
HyunSeo Nam <auspice7g@gmail.com>
2017-10-26 04:54:39 +0900

    Set root url to login

--- portfolium/urls.py
+++ portfolium/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from account.views import signin
 
 urlpatterns = [
-    url(r'^', include('main.urls', namespace='main')),
+    # url(r'^', include('main.urls', namespace='main')),
     url(r'^admin/', admin.site.urls),
     url(r'^portfolios/', include('portfolio.urls', namespace='portfolios')),
     url(r'^accounts/', include('account.urls', namespace='accounts')),
+    url(r'^', signin),
 ]
